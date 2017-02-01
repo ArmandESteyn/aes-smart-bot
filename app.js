e0367f044125ef1752c3aba14ab9a028bf102723
Unknown <ArmandS@fivefriday.com>
2017-02-01 09:39:27 +0200

    Work Dammit!

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var builder = require('botbuilder');
+var restify = require('restify');
 
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: 'f03bb295-c889-472e-b074-7131173c86c4',
@@ -7,27 +14,9 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-bot.dialog('/', dialog);
-
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
-var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/027ad2eb-64d6-45be-9818-9af0054c4e3f?subscription-key=e401b3bb9f4043d7b36982e6ab880664&q=hello&verbose=true';
-var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
-bot.dialog('/', dialog);
-
-// Add intent handlers
-dialog
-.matches('Greeting', [
-    function (session) {
-      
-         builder.Prompts.text(session,"Hello to you I am smart-bot");
-
-    }
-])
-.onDefault([
+bot.dialog('/', [
     function(session)
     {
-        builder.Prompts.text(session,"Sorry what ???");
+        builder.Prompts.text(session,"HELLOP");
     }
 ]);
-    
