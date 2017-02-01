2c0d2573c4e56db768d1aee47b87ddcd27fb4186
Unknown <ArmandS@fivefriday.com>
2017-02-01 11:17:01 +0200

    yo

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ bot.dialog('/', [
 
         var reply = 
         new builder.Message()
-        .addAttachment({contentType: 'image/gif', contentUrl: "./davie.gif" });
+        .addAttachment({contentType: 'image/gif', contentUrl: "https://raw.githubusercontent.com/ArmandESteyn/aes-smart-bot/master/davie.gif" });
          session.send(reply);
 
         //session.send('Hello %(name)s! I love %(company)s!', session.userData.profile);
