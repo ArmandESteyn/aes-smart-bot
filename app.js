a754221f554a5ca1050c65585aa7be8f347f65b6
Unknown <ArmandS@fivefriday.com>
2017-02-02 10:29:51 +0200

    Http Headers

--- app.js
+++ app.js
@@ -62,6 +62,36 @@ bot.dialog('/',intents);
       
        function(session)
        {
+           var options = 
+           {
+               url:'https://saluteapi.fivefriday.com/api/releasenotes',
+               headers:
+               {
+                   'Authorization':'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk1YzYyYTdhNDA5MDA0YTg5Zjk4NTAxNTM0YzgwZmU1IiwidHlwIjoiSldUIn0.eyJuYmYiOjE0ODYwMjM0NzAsImV4cCI6MTQ4NjAyNzA3MCwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50LmZpdmVmcmlkYXkuY29tIiwiYXVkIjpbImh0dHBzOi8vYWNjb3VudC5maXZlZnJpZGF5LmNvbS9yZXNvdXJjZXMiLCJTYWx1dGUiXSwiY2xpZW50X2lkIjoiU2FsdXRlSlMiLCJzdWIiOiJmOWEzZjA3Ny0wOWIxLTQ3MGQtYjRlMS02YzhkMjc3MWViN2IiLCJhdXRoX3RpbWUiOjE0ODYwMTY0NzYsImlkcCI6ImxvY2FsIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZ2l2ZW5uYW1lIjoiQXJtYW5kICIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3N1cm5hbWUiOiJTdGV5biIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6IkFybWFuZFNAZml2ZWZyaWRheS5jb20iLCJQcm9maWxlUGhvdG9VcmwiOiJodHRwczovL2ltYWdlcy5maXZlZnJpZGF5LmNvbS8vYXBpL2ltYWdlL2Y5MGZmNWUzLTQ0ZGMtNGVjMS03NDY2LTA4ZDQ0NGU2NmNkMiIsIlNhbHV0ZUNvbXBhbnkiOiJGaXZlRnJpZGF5Iiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsIlNhbHV0ZSJdLCJhbXIiOlsicHdkIl19.VpJAO1AxgtNrwGCaqKYbQ2SNpesJK9J_hui3GtuVemkVSj3sgqtHqwuy5HHiTdeGwC51g2NCHkRB57jE1XLpgSwMZNrLR2M8PZgvgCq7k63Eqct2TLwronyrDgcJCocnuR61UQubQIalPUp1p-WGMwZvmBYC506BLE37KKjBik4yun2PXyKWcpwzjBirngsVULW0rMFl283oJoccKkm_ZYezNayhQ3bgobybFERtbt2EqQm7Yl0us8XRRW8g8BwfJ9Dh67PKTxAhIFTzuEpypV9iEFNbYcGXzEu37eFt-yvA_3shQ7w-Z1-ee58xr7rfoeNuhnrWMy4bm7re-8UScg',
+                   'Company':'fivefriday'
+               }
+           };
+
+           function callback(error, response, body)
+           {
+               if(!error && response.statusCode == 200)
+               {
+                   var info = JSON.parse(body);
+                   session.send(info.body);
+               }
+           }
+       }
+
+    ])
+
+
+
+
+
+
+/*
+      function(session)
+       {
             request('https://salute.fivefriday.com/fivefriday/dashboard', function(error, response, body)
             {
                 if(!error)
@@ -75,7 +105,6 @@ bot.dialog('/',intents);
             });
             session.send("I got your back jack");
        }
-    ])
 
     
-
+*/
