b9baaa9e704931271cedb21f281137804f3b0c47
Unknown <ArmandS@fivefriday.com>
2017-02-02 13:45:59 +0200

    this is not gonna work

--- app.js
+++ app.js
@@ -2,6 +2,49 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var request = require('request');
 
+
+
+
+var settings = {
+   authority: 'https://account-dev.fivefriday.com/',
+   client_id: 'SaluteJS',
+   redirect_uri: 'http://localhost:3000/oidc-client-sample.html',
+   post_logout_redirect_uri: 'http://localhost:3000/oidc-client-sample.html',
+   response_type: 'id_token token',
+   scope: 'openid profile Salute',
+   acr_values: 'tenant:salute',
+   filterProtocolClaims: true,
+   loadUserInfo: true
+};
+
+
+var client = new Oidc.OidcClient(settings);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/172b2185-8a3d-4ef2-b46d-18107e453c7c?subscription-key=e401b3bb9f4043d7b36982e6ab880664&verbose=true';
 var recognizer = new builder.LuisRecognizer(model);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] });
@@ -60,32 +103,18 @@ bot.dialog('/',intents);
     ])
    .matches('shots',[
       
-       function(session)
-       {
-           var options = {
-               url:'https://saluteapi.fivefriday.com/api/releasenotes',
-               headers:{
-                   'Authorization':'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk1YzYyYTdhNDA5MDA0YTg5Zjk4NTAxNTM0YzgwZmU1IiwidHlwIjoiSldUIn0.eyJuYmYiOjE0ODYwMjM0NzAsImV4cCI6MTQ4NjAyNzA3MCwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50LmZpdmVmcmlkYXkuY29tIiwiYXVkIjpbImh0dHBzOi8vYWNjb3VudC5maXZlZnJpZGF5LmNvbS9yZXNvdXJjZXMiLCJTYWx1dGUiXSwiY2xpZW50X2lkIjoiU2FsdXRlSlMiLCJzdWIiOiJmOWEzZjA3Ny0wOWIxLTQ3MGQtYjRlMS02YzhkMjc3MWViN2IiLCJhdXRoX3RpbWUiOjE0ODYwMTY0NzYsImlkcCI6ImxvY2FsIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZ2l2ZW5uYW1lIjoiQXJtYW5kICIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3N1cm5hbWUiOiJTdGV5biIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6IkFybWFuZFNAZml2ZWZyaWRheS5jb20iLCJQcm9maWxlUGhvdG9VcmwiOiJodHRwczovL2ltYWdlcy5maXZlZnJpZGF5LmNvbS8vYXBpL2ltYWdlL2Y5MGZmNWUzLTQ0ZGMtNGVjMS03NDY2LTA4ZDQ0NGU2NmNkMiIsIlNhbHV0ZUNvbXBhbnkiOiJGaXZlRnJpZGF5Iiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsIlNhbHV0ZSJdLCJhbXIiOlsicHdkIl19.VpJAO1AxgtNrwGCaqKYbQ2SNpesJK9J_hui3GtuVemkVSj3sgqtHqwuy5HHiTdeGwC51g2NCHkRB57jE1XLpgSwMZNrLR2M8PZgvgCq7k63Eqct2TLwronyrDgcJCocnuR61UQubQIalPUp1p-WGMwZvmBYC506BLE37KKjBik4yun2PXyKWcpwzjBirngsVULW0rMFl283oJoccKkm_ZYezNayhQ3bgobybFERtbt2EqQm7Yl0us8XRRW8g8BwfJ9Dh67PKTxAhIFTzuEpypV9iEFNbYcGXzEu37eFt-yvA_3shQ7w-Z1-ee58xr7rfoeNuhnrWMy4bm7re-8UScg',
-                   'Company':'fivefriday'
-               }
-           };
+      function(session)
+      {
+            client.createSigninRequest({state: { bar: 15}}).then(function(req)
+            {
+                session.send("signin request", req,"<a href='"+req.url+"'>go signup</a> ");
 
-          function callback(error, response, body){
+               window.location = req.url;
 
-               if(!error && response.statusCode == 200){
+            })
+          
 
-                   var info = JSON.parse(body);
-                   session.send(info[0].summary);
-               }
-               else
-               {
-                   session.send("Something went wrong");
-               }
-           }
-
-           request(options,callback);
-
-       }
+      }
 
     ])
 
@@ -115,4 +144,34 @@ bot.dialog('/',intents);
        }
 
     
+*/
+
+
+/*
+ function(session)
+       {
+           var options = {
+               url:'https://saluteapi.fivefriday.com/api/releasenotes',
+               headers:{
+                   'Authorization':'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk1YzYyYTdhNDA5MDA0YTg5Zjk4NTAxNTM0YzgwZmU1IiwidHlwIjoiSldUIn0.eyJuYmYiOjE0ODYwMjM0NzAsImV4cCI6MTQ4NjAyNzA3MCwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50LmZpdmVmcmlkYXkuY29tIiwiYXVkIjpbImh0dHBzOi8vYWNjb3VudC5maXZlZnJpZGF5LmNvbS9yZXNvdXJjZXMiLCJTYWx1dGUiXSwiY2xpZW50X2lkIjoiU2FsdXRlSlMiLCJzdWIiOiJmOWEzZjA3Ny0wOWIxLTQ3MGQtYjRlMS02YzhkMjc3MWViN2IiLCJhdXRoX3RpbWUiOjE0ODYwMTY0NzYsImlkcCI6ImxvY2FsIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZ2l2ZW5uYW1lIjoiQXJtYW5kICIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3N1cm5hbWUiOiJTdGV5biIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6IkFybWFuZFNAZml2ZWZyaWRheS5jb20iLCJQcm9maWxlUGhvdG9VcmwiOiJodHRwczovL2ltYWdlcy5maXZlZnJpZGF5LmNvbS8vYXBpL2ltYWdlL2Y5MGZmNWUzLTQ0ZGMtNGVjMS03NDY2LTA4ZDQ0NGU2NmNkMiIsIlNhbHV0ZUNvbXBhbnkiOiJGaXZlRnJpZGF5Iiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsIlNhbHV0ZSJdLCJhbXIiOlsicHdkIl19.VpJAO1AxgtNrwGCaqKYbQ2SNpesJK9J_hui3GtuVemkVSj3sgqtHqwuy5HHiTdeGwC51g2NCHkRB57jE1XLpgSwMZNrLR2M8PZgvgCq7k63Eqct2TLwronyrDgcJCocnuR61UQubQIalPUp1p-WGMwZvmBYC506BLE37KKjBik4yun2PXyKWcpwzjBirngsVULW0rMFl283oJoccKkm_ZYezNayhQ3bgobybFERtbt2EqQm7Yl0us8XRRW8g8BwfJ9Dh67PKTxAhIFTzuEpypV9iEFNbYcGXzEu37eFt-yvA_3shQ7w-Z1-ee58xr7rfoeNuhnrWMy4bm7re-8UScg',
+                   'Company':'fivefriday'
+               }
+           };
+
+          function callback(error, response, body){
+
+               if(!error && response.statusCode == 200){
+
+                   var info = JSON.parse(body);
+                   session.send(info[0].summary);
+               }
+               else
+               {
+                   session.send("Something went wrong");
+               }
+           }
+
+           request(options,callback);
+
+       }
 */
